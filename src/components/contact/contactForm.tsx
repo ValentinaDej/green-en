5a78cac7431d53c49604ea64669a30dc27a27c20
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-12-02 18:35:08 +0200

    contact us form

--- src/components/contact/contactForm.tsx
+++ src/components/contact/contactForm.tsx
@@ -1,9 +1,71 @@
-import Plus from "@/shared/icons/Plus";
-import Minus from "@/shared/icons/Minus";
-import classes from "./contactCard.module.css";
+import React from "react";
+import classes from "./contactForm.module.css";
 
-const ContactForm = () => {
-  return <div>ContactForm</div>;
-};
+function ContactForm() {
+  return (
+    <div className={classes.contactContainer}>
+      <form className={classes.form}>
+        <div className={classes.formGroup}>
+          <label htmlFor="fullName" className={classes.label}>
+            Full name:
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            placeholder="John Rosie"
+            className={classes.input}
+            required
+          />
+        </div>
+
+        <div className={classes.formGroup}>
+          <label htmlFor="email" className={classes.label}>
+            E-mail:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="johnrosie@gmail.com"
+            className={classes.input}
+            required
+          />
+        </div>
+
+        <div className={classes.formGroup}>
+          <label htmlFor="phone" className={classes.label}>
+            Phone:
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="380961234567"
+            className={classes.input}
+            pattern="\d{10,}"
+            required
+          />
+        </div>
+
+        <div className={classes.formGroup}>
+          <label htmlFor="message" className={classes.label}>
+            Message:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your message"
+            className={classes.textarea}
+          ></textarea>
+        </div>
+
+        <button type="button" className={classes.button}>
+          Send <span className={classes.arrow}>→</span>
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default ContactForm;
