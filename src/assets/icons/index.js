23dc91cac03574328c19c874e5fd3be9c9362713
ValentinaDej <107748584+ValentinaDej@users.noreply.github.com>
2023-12-04 11:42:25 +0200

    read me

--- src/assets/icons/index.js
+++
@@ -1,4 +0,0 @@
-import hamburger from "./hamburger.svg";
-import arrowRight from "./arrow-right.svg";
-
-export { hamburger, arrowRight };
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,14 +1,13 @@
 import logo from "./header-logo.svg";
-
 import banerImage from "./wind-turbine-clean-energy.png";
 
-import dudaRhoto from "./Duds.png";
-import dunaevPhoto from "./Dunaev.png";
-import shevchenkoPhoto from "./Shevchenko.png";
-import turchenkoPhoto from "./Turchenko.png";
+import dudaRhoto from "./team/Duds.png";
+import dunaevPhoto from "./team/Dunaev.png";
+import shevchenkoPhoto from "./team/Shevchenko.png";
+import turchenkoPhoto from "./team/Turchenko.png";
 
-import emploee from "./emploee.png";
-import solarPanels from "./solar-panels.png";
+import emploee from "./values/emploee.png";
+import solarPanels from "./values/solar-panels.png";
 
 import buh from "./cases/buh.png";
 import kyiv from "./cases/kyiv.png";
